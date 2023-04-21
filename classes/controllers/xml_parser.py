5cf146e09dad31a5fe408121afcbca02421036e6
lukashdias <lukas.dias@grupomateus.com.br>
2023-04-21 18:09:58 -0300

    chore: hash inverso

--- classes/controllers/xml_parser.py
+++ classes/controllers/xml_parser.py
@@ -18,13 +18,14 @@ class XmlParser:
         # Se não houver xml_dir, retorne 0
         if not xml_dir:
             raise ValueError('diretório do xml inválido ou não encontrado')
-        
+
         # Carrega o xml
         tree = xml.parse(xml_dir)
 
         # Agora o root corresponde ao xml
         self.root = tree.getroot()
         self.memoized_searches = dict()
+        self.memoized_reverse_searches = dict()
 
     def get_all_pages(self) -> List[Page]:
         # Se não houver root
@@ -43,23 +44,39 @@ class XmlParser:
             raise ValueError('xml_root inválido ou não encontrado')
         if not tag:
             raise ValueError('tag inválida ou não encontrada')
+
         # Traz todas as páginas
         pages = self.root.findall('page')
+
         # Filtra as páginas que contém a tag
         tags = filter(lambda page: page.find(tag) is not None, pages)
-        # Retorna uma lista com o texto da tag
-        return list(map(lambda page: page.find(tag).text, tags)) # type: ignore
 
-    def search_for_term(self, term: str,  tag= 'title') -> list:
+        # Retorna uma lista com o texto da tag
+        mapped_tags = map(lambda page: page.find(tag).text, tags)
+
+        if mapped_tags is not None:
+            return list(mapped_tags)
+
+        return []
+
+    def search_for_term(self, term: str, hash_method: Literal['reverse', 'default'] = 'default') -> list:
         # Se não houver root
         if not self.root:
             raise ValueError('self.root inválido ou não encontrado')
         # Se não houver termo
         if not term:
             raise ValueError('termo inválido ou não encontrado')
-        
-        if hash(term) in self.memoized_searches:
-            return self.memoized_searches[hash(term)]
+
+        if (hash_method == 'reverse'):
+            # chave = lista de páginas, valor = hash do termo
+            for k, v in self.memoized_reverse_searches.items():
+                if v == hash(term):
+                    print('Hash invertido')
+                    return sorted(k, reverse=True)
+        else:
+            if hash(term) in self.memoized_searches:
+                print('Hash padrao')
+                return sorted(self.memoized_searches[hash(term)], reverse=True)
 
         # Lista de resultados
         included: List[Page] = []
@@ -78,22 +95,21 @@ class XmlParser:
                 # Multiplique a relevância por 2 se o termo estiver no título
                 if isOnTitle:
                     count *= 2
-                temp_page = Page(page.id, page.title, page.text, count * 2 if isOnTitle else count)
+                temp_page = Page(page.id, page.title, page.text,
+                                 count * 2 if isOnTitle else count)
                 included.append(temp_page)
         result = sorted(included, reverse=True)
 
         self.memoize_search(term, result)
-        
-        return sorted(included, reverse=True)
 
+        return sorted(included, reverse=True)
 
     def get_memorized_searches(self) -> dict:
         return self.memoized_searches
-    
+
     def memoize_search(self, term: str, pages: List[Page], method: Literal['reverse', 'default'] = 'default') -> None:
         if method == 'default':
             self.memoized_searches[hash(term)] = pages
-        else: 
+        else:
             self.memoized_reverse_searches[pages] = hash(term)
         pass
-        
